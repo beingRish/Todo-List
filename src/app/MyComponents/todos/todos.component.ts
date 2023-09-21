61557363e627e832933b5b88fcb15150e45e9d36
Rishabh Singh <rishabhjuly11@gmail.com>
2023-09-21 10:55:26 +0530

    generated component to add Todo

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -39,5 +39,7 @@ export class TodosComponent implements OnInit {
 
   deleteTodo(todo:Todo){
     console.log(todo);
+    const index = this.todos.indexOf(todo);
+    this.todos.splice(index, 1);
   }
 }
